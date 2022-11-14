ea074bdfddfd12d3ee6adabc2092de5562176a64
nome_usuario_aqui <email_github_aqui@gmail.com>
2022-11-14 13:52:30 -0300

    up

--- RSGARCH_DGP.jl
+++ RSGARCH_DGP.jl
@@ -12,13 +12,15 @@ n = 1000
 distri = Normal()
 c = [0.4, 0.6]
 d = [0,8, 0.3]
+P = [0.9 0.1; 0.2 0.8];
+time_varying = false;
 
-simulate_gray = function(n = 1000, distribution = "std", omega, alpha, beta, time_varying = true, P = nothing, c = nothing, d = nothing, burnin = 500)
+function simulate_gray(n, omega, alpha, beta, burnin, time_varying, P, c, d)
    
    if (!time_varying & isnothing(P)) 
     @error "Transition matrix P should be provided"
    end
-   if (time_varying & (isnothing(C) || isnothing(D)))
+   if (time_varying & (isnothing(c) || isnothing(c)))
     @error "Vectors c and d should be provided"
    end
 
@@ -27,8 +29,8 @@ simulate_gray = function(n = 1000, distribution = "std", omega, alpha, beta, tim
    e = rand(Normal(), ntot);
    h = zeros(ntot, k + 1);
    r = zeros(ntot);
-   Pt = zeros(ntot, k);
-   h[1, 1:k] = 1;
+   Pt = zeros(ntot);
+   h[1, 1:k] .= 1.0;
 
    if (!time_varying)
     p = P[1, 1];
@@ -38,34 +40,35 @@ simulate_gray = function(n = 1000, distribution = "std", omega, alpha, beta, tim
     r[1] = e[1] * sqrt(h[1, k + 1]);
     for i = 2:ntot
         h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[1, k + 1];
-        numA = (1 - q) * pdf(Normal(0, sqrt(h[t - 1, 2])), r[t - 1]) * (1 - Pt[i - 1, 1]);
-        numB = p * pdf(Normal(0, sqrt(h[t - 1, 1])), r[t - 1]) * Pt[t - 1, 1];
-        deno = pdf(Normal(0, sqrt(h[t - 1, 1])), r[t - 1]) * Pt[t - 1, 1] + 
-               pdf(Normal(0, sqrt(h[t - 1, 2])), r[t - 1]) * (1 - Pt[t - 1, 1]);
+        numA = (1 - q) * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1, 1]);
+        numB = p * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1, 1];
+        deno = pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1, 1] + 
+               pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1, 1]);
         Pt[i] = numA/deno + numB/deno;
         h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
         r[i] = e[i] * sqrt(h[i, k + 1]);
     end
-   elseif
+   else
     p = cdf(Normal(), c[1] + d[1] * 0);
-    q = cdf(Normal(), c[1] + d[1] * 0);
+    q = cdf(Normal(), c[2] + d[2] * 0);
     Pt[1] = (1 - q) / (2 - p - q);
     h[1, k + 1] = Pt[1] * h[1, 1] + (1 - Pt[1]) * h[1, 2];
     r[1] = e[1] * sqrt(h[1, k + 1]);
     for i = 2:ntot
         p = cdf(Normal(), c[1] + d[1] * r[i - 1]);
-        q = cdf(Normal(), c[1] + d[1] * r[i - 1]);
+        q = cdf(Normal(), c[2] + d[2] * r[i - 1]);
         h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[1, k + 1];
-        numA = (1 - q) * pdf(Normal(0, sqrt(h[t - 1, 2])), r[t - 1]) * (1 - Pt[i - 1, 1]);
-        numB = p * pdf(Normal(0, sqrt(h[t - 1, 1])), r[t - 1]) * Pt[t - 1, 1];
-        deno = pdf(Normal(0, sqrt(h[t - 1, 1])), r[t - 1]) * Pt[t - 1, 1] + 
-               pdf(Normal(0, sqrt(h[t - 1, 2])), r[t - 1]) * (1 - Pt[t - 1, 1]);
+        numA = (1 - q) * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1, 1]);
+        numB = p * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1, 1];
+        deno = pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1, 1] + 
+               pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1, 1]);
         Pt[i] = numA/deno + numB/deno;
         h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
         r[i] = e[i] * sqrt(h[i, k + 1]);
     end
    end
-   return r = r[burnin + 1:end], h = h[burnin + 1:end, ], Pt = Pt[burnin + 1: end, ]
+   #return r[burnin + 1:end];
+   return r[burnin + 1:end], h[burnin + 1:end, ], Pt[burnin + 1: end, ];
 end
 
 
@@ -96,4 +99,4 @@ simulate_haas = function(n, omega, alpha, beta, P)
 end
 
 
-teste = simulate_haas(1000, omega, alpha, beta, P)
+teste_r, teste_h, teste_p = simulate_gray(1000, omega, alpha, beta, 500, true, nothing, c, d)
