b6674f0d55625dfe0c9f2a10549c3d4be7e1502f
nome_usuario_aqui <email_github_aqui@gmail.com>
2022-12-04 09:31:28 -0300

    Scales-T

--- Julia/examples.jl
+++ Julia/examples.jl
@@ -1,7 +1,7 @@
 ##################################################
 ###     How the functions should be used      ####
 ##################################################
-using Distributions, Optim, ForwardDiff, StatsFuns, LinearAlgebra, Statistics, JuMP, BenchmarkTools
+using Distributions, Optim, ForwardDiff, StatsFuns, LinearAlgebra, Statistics, JuMP, BenchmarkTools, Plots
 
 include("gray_dgp.jl")
 include("gray_ml.jl")
@@ -14,11 +14,12 @@ alpha  = [0.4, 0.1];
 beta = [0.2, 0.7];
 P = [0.9 0.03; 0.1 0.97];
 time_varying = false;
+distri = "std";
 C = 1;
 D = 1;
 k = 2;
 burnin = 500;
-(r, h, Pt, s) = simulate_gray(n, omega, alpha, beta, time_varying, P, C, D, burnin);
+(r, h, Pt, s) = simulate_gray(n, distri, omega, alpha, beta, time_varying, P, C, D, burnin);
 par_ini = [0.1, 0.01, 0.3, 0.1, 0.5, 0.3, 0.8, 0.95];
 theta_hat = fit_gray(r, k, par_ini)
 
--- Julia/gray_dgp.jl
+++ Julia/gray_dgp.jl
@@ -3,17 +3,18 @@
 #################################################
 using Random, Distributions
 
-function simulate_gray(n, omega, alpha, beta, time_varying, P, C, D, burnin)
+function simulate_gray(n, distri, omega, alpha, beta, time_varying, P, C, D, burnin)
    if (!time_varying & isnothing(P)) 
     @error "Transition matrix P should be provided"
    end
    if (time_varying & (isnothing(C) || isnothing(D)))
-    @error "Vectors c and d should be provided"
+    @error "Vectors C and D should be provided"
    end
+   
 
    ntot = n + burnin;
    k = length(omega);
-   e = rand(Normal(), ntot);
+   e = ifelse(distri == "norm", rand(Normal(), ntot), sqrt(5/7).* rand(TDist(7), ntot));
    h = zeros(ntot, k + 1);
    r = zeros(ntot);
    s = Vector{Int32}(undef, ntot);
@@ -27,16 +28,30 @@ function simulate_gray(n, omega, alpha, beta, time_varying, P, C, D, burnin)
     h[1, k + 1] = Pt[1] * h[1, 1] + (1 - Pt[1]) * h[1, 2];
     s[1] = wsample([1, 2], [Pt[1], 1 - Pt[1]], 1)[1];
     r[1] = e[1] * sqrt(h[1, s[1]]);
-    for i = 2:ntot
-        h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[i - 1, k + 1];
-        numA = (1 - q) * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
-        numB = p * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1];
-        deno = pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] + 
-               pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
-        Pt[i] = numA/deno + numB/deno;
-        h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
-        s[i] = wsample([1, 2], [Pt[i], 1 - Pt[i]], 1)[1];
-        r[i] = e[i] * sqrt(h[i, s[i]]);
+    if distri == "norm"
+        for i = 2:ntot
+            h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[i - 1, k + 1];
+            numA = (1 - q) * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
+            numB = p * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1];
+            deno = pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] + 
+                   pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
+            Pt[i] = numA/deno + numB/deno;
+            h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
+            s[i] = wsample([1, 2], [Pt[i], 1 - Pt[i]], 1)[1];
+            r[i] = e[i] * sqrt(h[i, s[i]]);
+        end
+    else
+        for i = 2:ntot
+            h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[i - 1, k + 1];
+            numA = (1 - q) * sqrt(7/5) / sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 2])) * (1 - Pt[i - 1]);
+            numB = p * sqrt(7/5) / sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 1])) * Pt[i - 1];
+            deno = sqrt(7/5) / sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 1])) * Pt[i - 1] + 
+            sqrt(7/5) / sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 2])) * (1 - Pt[i - 1]);
+            Pt[i] = numA/deno + numB/deno;
+            h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
+            s[i] = wsample([1, 2], [Pt[i], 1 - Pt[i]], 1)[1];
+            r[i] = e[i] * sqrt(h[i, s[i]]);
+        end
     end
    else
     p = cdf(Normal(), C[1] + D[1] * 0);              ## 0 é a melhor opção?
@@ -45,21 +60,37 @@ function simulate_gray(n, omega, alpha, beta, time_varying, P, C, D, burnin)
     h[1, k + 1] = Pt[1] * h[1, 1] + (1 - Pt[1]) * h[1, 2];
     s[1] = wsample([1, 2], [Pt[1], 1 - Pt[1]], 1)[1];
     r[1] = e[1] * sqrt(h[1, s[1]]);
-    for i = 2:ntot
-        p = cdf(Normal(), C[1] + D[1] * r[i - 1]);
-        q = cdf(Normal(), C[2] + D[2] * r[i - 1]);
-        h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[i - 1, k + 1];
-        numA = (1 - q) * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
-        numB = p * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1];
-        deno = pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] + 
-               pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
-        Pt[i] = numA/deno + numB/deno;
-        h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
-        s[i] = wsample([1, 2], [Pt[i], 1 - Pt[i]], 1)[1];
-        r[i] = e[i] * sqrt(h[i, s[i]]);
+    if distri == "norm"
+        for i = 2:ntot
+            p = cdf(Normal(), C[1] + D[1] * r[i - 1]);
+            q = cdf(Normal(), C[2] + D[2] * r[i - 1]);
+            h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[i - 1, k + 1];
+            numA = (1 - q) * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
+            numB = p * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1];
+            deno = pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] + 
+                   pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]);
+            Pt[i] = numA/deno + numB/deno;
+            h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
+            s[i] = wsample([1, 2], [Pt[i], 1 - Pt[i]], 1)[1];
+            r[i] = e[i] * sqrt(h[i, s[i]]);
+        end
+    else
+        for i = 2:ntot
+            p = cdf(Normal(), C[1] + D[1] * r[i - 1]);
+            q = cdf(Normal(), C[2] + D[2] * r[i - 1]);
+            h[i, 1:k] = omega .+ alpha.* r[i - 1]^2 + beta.* h[i - 1, k + 1];
+            numA = (1 - q) * sqrt(7/5) / sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 2])) * (1 - Pt[i - 1]);
+            numB = p * sqrt(7/5) / sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 1])) * Pt[i - 1];
+            deno = sqrt(7/5) / sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 1])) * Pt[i - 1] + 
+            sqrt(7/5) / sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1] * sqrt(7/5) / sqrt(h[i - 1, 2])) * (1 - Pt[i - 1]);
+            Pt[i] = numA/deno + numB/deno;
+            h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
+            s[i] = wsample([1, 2], [Pt[i], 1 - Pt[i]], 1)[1];
+            r[i] = e[i] * sqrt(h[i, s[i]]);
+        end
     end
    end
-   return r[burnin + 1:end], h[burnin + 1:end, ], Pt[burnin + 1: end, ], s[burnin + 1: end];
+   return r[burnin + 1: end], h[burnin + 1: end, :], Pt[burnin + 1: end], s[burnin + 1: end];
 end
 
 
