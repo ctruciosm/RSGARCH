9dc74e6656a0278316da952e898fd4a64c6593f2
ctruciosm <ctrucios@gmail.com>
2022-12-16 07:15:06 -0300

    up

--- Julia/gray_ml.jl
+++ Julia/gray_ml.jl
@@ -2,7 +2,7 @@
 ###  RSGARCH Estim: Estimate RSGARCH Models   ####
 ##################################################
  
-function gray_likelihood(par, r, k, distri)
+function gray_likelihood(r::Vector{Float64}, k::Int64, distri::String, par)
     # par(numeric vector): [ω, α, β, p11, p22, 1/gl]
     n = length(r);
     h = Matrix{Float64}(undef, n, k + 1);
@@ -16,11 +16,11 @@ function gray_likelihood(par, r, k, distri)
     q = par[4 * k];
 
     if sum(α + β .< 1) == 2
-        Pt[1] = (1 - q) / (2 - p - q);              ## Pi = P(St = 1) - Pag 683 Hamilton (1994)
-        h[1, 1:k] .= var(r[1:30]);                        
+        Pt[1] = (1 - q) / (2 - p - q);              
+        h[1, 1:k] .= var(r);                        
         h[1, k + 1] = Pt[1] * h[1, 1] + (1 - Pt[1]) * h[1, 2];
         if distri == "norm"
-            for i = 2:n
+            @inbounds for i = 2:n
                 h[i, 1:k] = ω .+ α .* r[i - 1]^2 + β .* h[i - 1, k + 1];
                 Pt[i] = probability_regime_given_time_n(p, q, sqrt.(h[i - 1, :]), r[i - 1], Pt[i - 1]);
                 h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
@@ -29,7 +29,7 @@ function gray_likelihood(par, r, k, distri)
         else
             η = par[4 * k + 1];
             ν = 1/η;
-            for i = 2:n
+            @inbounds for i = 2:n
                 h[i, 1:k] = ω .+ α .* r[i - 1]^2 + β .* h[i - 1, k + 1];
                 Pt[i] = probability_regime_given_time_t(p, q, sqrt.(h[i- 1, :]), r[i - 1], Pt[i - 1], ν);
                 h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
@@ -43,28 +43,28 @@ function gray_likelihood(par, r, k, distri)
 end
 
 
-function fit_gray(r, k, par_ini, distri)
+function fit_gray(r::Vector{Float64}, k::Int64, par_ini, distri::String)
     if distri == "norm"
         if isnothing(par_ini)
             par_ini = [0.05, 0.15, 0.3, 0.1, 0.6, 0.2, 0.85, 0.92];     
-            ll = gray_likelihood(par_ini, r, k, distri);
+            ll = gray_likelihood(r, k, distri, par_ini);
             for i in 1:1000                                 
                 ω = rand(Uniform(0.0001, 0.4), k);
                 α = rand(Uniform(0.01, 0.6), k);
                 β = [rand(Uniform(0.1, 1 - α[1] - 1e-6), 1); rand(Uniform(0.1, 1 - α[2] - 1e-6), 1)];
                 p = rand(Uniform(0.8, 0.99), k);
                 par_random = [ω; α; β; p];
-                if gray_likelihood(par_random, r, k, distri) < ll
-                    ll = gray_likelihood(par_random, r, k, distri);
+                if gray_likelihood(r, k, distri, par_random) < ll
+                    ll = gray_likelihood(r, k, distri, par_random);
                     par_ini = par_random;
                 end
             end
         end    
-        optimum = optimize(par -> gray_likelihood(par, r, k, distri), [1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6 ,1e-6 ,1e-6], [Inf, Inf, 1, 1, 1, 1, 1, 1], par_ini);
+        optimum = optimize(par -> gray_likelihood(r, k, distri, par), [1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6 ,1e-6 ,1e-6], [Inf, Inf, 1, 1, 1, 1, 1, 1], par_ini);
     else
         if isnothing(par_ini)
             par_ini = [0.05, 0.15, 0.3, 0.1, 0.6, 0.2, 0.85, 0.92, 0.2];    
-            ll = gray_likelihood(par_ini, r, k, distri);
+            ll = gray_likelihood(r, k, distri, par_ini);
             for i in 1:1000                                 
                 ω = rand(Uniform(0.0001, 0.4), k);
                 α = rand(Uniform(0.01, 0.6), k);
@@ -72,13 +72,13 @@ function fit_gray(r, k, par_ini, distri)
                 p = rand(Uniform(0.8, 0.99), k);
                 η = rand(Uniform(0.01, 0.5), 1);
                 par_random = [ω; α; β; p; η];
-                if gray_likelihood(par_random, r, k, distri) < ll
-                    ll = gray_likelihood(par_random, r, k, distri);
+                if gray_likelihood(r, k, distri, par_random) < ll
+                    ll = gray_likelihood(r, k, distri, par_random);
                     par_ini = par_random;
                 end
             end
         end
-        optimum = optimize(par -> gray_likelihood(par, r, k, distri), [1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6 ,1e-6 ,1e-6, 1e-2], [Inf, Inf, 1, 1, 1, 1, 1, 1, 0.5], par_ini);
+        optimum = optimize(par -> gray_likelihood(r, k, distri, par), [1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6 ,1e-6 ,1e-6, 1e-2], [Inf, Inf, 1, 1, 1, 1, 1, 1, 0.5], par_ini);
     end
     mle = optimum.minimizer;
     return mle;
--- Julia/utils.jl
+++ Julia/utils.jl
@@ -2,21 +2,21 @@
 ###       Auxiliary Functions       #####
 ######################################### 
 
-function Tstudent(x, η)
+function Tstudent(x::Real, η::Real)
     a = (gamma((1 + η) / (2 * η)))/(sqrt(π * (1 - 2 * η)/η) * gamma(1 / (2 * η)));
     b = (1 + η * x^2/(1 - 2 * η))^((η + 1)/(2 * η));
     return a/b;
 end
 
 
-function probability_regime_given_time_n(p, q, σ, r, Pt)
+function probability_regime_given_time_n(p::Real, q::Real, σ::Vector{Float64}, r::Real, Pt::Real)
     numA = (1 - q) * pdf(Normal(0, σ[2]), r) * (1 - Pt);
     numB = p * pdf(Normal(0, σ[1]), r) * Pt;
     deno = pdf(Normal(0, σ[1]), r) * Pt + pdf(Normal(0, σ[2]), r) * (1 - Pt);
     return numA/deno + numB/deno;
 end
 
-function probability_regime_given_time_t(p, q, σ, r, Pt, ν)
+function probability_regime_given_time_t(p::Real, q::Real, σ::Vector{Float64}, r::Real, Pt::Real, ν::Real)
     numA = (1 - q) * sqrt(ν/(ν-2))/σ[2] * pdf(TDist(ν), r*sqrt(ν/(ν-2))/σ[2]) * (1 - Pt);
     numB = p * sqrt(ν/(ν-2))/σ[1] * pdf(TDist(ν), r*sqrt(ν/(ν-2))/σ[1]) * Pt;
     deno = sqrt(ν/(ν-2))/σ[1] * pdf(TDist(ν), r*sqrt(ν/(ν-2))/σ[1]) * Pt + sqrt(ν/(ν-2))/σ[2] * pdf(TDist(ν), r*sqrt(ν/(ν-2))/σ[2]) * (1 - Pt);
