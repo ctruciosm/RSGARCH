bb0a716104410ad9980bdb6cbbcba27036663047
ctruciosm <ctrucios@gmail.com>
2023-02-18 05:32:43 -0300

    DGP Klaassen

--- Julia/DGP.jl
+++ Julia/DGP.jl
@@ -76,6 +76,45 @@ function simulate_haas(n, distri, ω, α, β, P, burnin)
     return r[burnin + 1: end - 1], h[burnin + 1: end, :], Pt[burnin + 1: end], s[burnin + 1: end];
 end
 #################################################
+function simulate_klaassen(n, distri, ω, α, β, P, burnin)
+    # P = [p11, 1 - p22; 1 - p11, p22]
+    ntot = n + burnin + 1;
+    k = length(ω);
+    h = Matrix{Float64}(undef, ntot, k + 1);
+    r = Vector{Float64}(undef, ntot);
+    s = Vector{Int32}(undef, ntot);
+    Pt = Vector{Float64}(undef, ntot);
+
+    e = ifelse(distri == "norm", rand(Normal(), ntot), sqrt(5/7).* rand(TDist(7), ntot));
+    h[1, 1:k] .= 1.0;
+    p = P[1, 1];
+    q = P[2, 2];
+    Pt[1] = (1 - q) / (2 - p - q);  
+    h[1, k + 1] = 1.0;
+    s[1] = wsample([1, 2], [Pt[1], 1 - Pt[1]])[1];
+    r[1] = e[1] * sqrt(h[1, s[1]]);
+    if distri == "norm"
+        for i = 2:ntot
+            Pt[i] = probability_regime_given_time_n(p, q, sqrt.(h[i - 1, :]), r[i - 1], Pt[i - 1]);
+            h[i, 1] = ω[1] + α[1] * r[i - 1]^2 + β[1] * (P[1,1] * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] * h[i - 1, 1] + P[2,1] * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]) * h[i - 1, 2])/(Pt[i] * (pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] + pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1])));
+            h[i, 2] = ω[2] + α[2] * r[i - 1]^2 + β[2] * (P[1,2] * pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] * h[i - 1, 1] + P[2,2] * pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1]) * h[i - 1, 2])/((1 - Pt[i]) * (pdf(Normal(0, sqrt(h[i - 1, 1])), r[i - 1]) * Pt[i - 1] + pdf(Normal(0, sqrt(h[i - 1, 2])), r[i - 1]) * (1 - Pt[i - 1])));
+            h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
+            s[i] = wsample([1, 2], P[:, s[i-1]])[1]; 
+            r[i] = e[i] * sqrt(h[i, s[i]]);
+        end
+    else
+        for i = 2:ntot
+            Pt[i] = probability_regime_given_time_t(p, q, sqrt.(h[i - 1, :]), r[i- 1], Pt[i - 1], 7);
+            h[i, 1] = ω[1] + α[1] * r[i - 1]^2 + β[1] * (P[1,1] * (sqrt(7/5)/sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 1]))) * Pt[i - 1] * h[i - 1, 1] + P[2,1] * (sqrt(7/5)/sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 2]))) * (1 - Pt[i - 1]) * h[i - 1, 2])/(Pt[i] * ((sqrt(7/5)/sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 1]))) * Pt[i - 1] + (sqrt(7/5)/sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 2]))) * (1 - Pt[i - 1])));
+            h[i, 2] = ω[2] + α[2] * r[i - 1]^2 + β[2] * (P[1,2] * (sqrt(7/5)/sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 1]))) * Pt[i - 1] * h[i - 1, 1] + P[2,2] * (sqrt(7/5)/sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 2]))) * (1 - Pt[i - 1]) * h[i - 1, 2])/((1 - Pt[i]) * ((sqrt(7/5)/sqrt(h[i - 1, 1]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 1]))) * Pt[i - 1] + (sqrt(7/5)/sqrt(h[i - 1, 2]) * pdf(TDist(7), r[i - 1]*sqrt(7/5)/sqrt(h[i - 1, 2]))) * (1 - Pt[i - 1])));
+            h[i, k + 1] = Pt[i] * h[i, 1] + (1 - Pt[i]) * h[i, 2];
+            s[i] = wsample([1, 2], P[:, s[i-1]])[1];
+            r[i] = e[i] * sqrt(h[i, s[i]]);
+        end
+    end
+    return r[burnin + 1: end - 1], h[burnin + 1: end, :], Pt[burnin + 1: end], s[burnin + 1: end];
+end
+#################################################
 function simulate_garch(n, ω, α, β, burnin)
     ntot = n + burnin + 1;
     h = Vector{Float64}(undef, ntot);
